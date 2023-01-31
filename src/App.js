519ed85bce4fff0a5958d55b0f4b841155c86384
OSAMA SHAIKH <shaikhosama478@gmail.com>
2023-01-31 17:00:31 +0530

    ✅ data fetched

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
-import { SiTailwindcss } from "react-icons/si";
+import { useEffect, useState } from "react";
+
 import CharacterCard from "./components/CharacterCard";
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
 
 const App = () => {
+	const [loadedData, setLoadedData] = useState({});
+	// const [isLoading, setIsLoading] = useState(false);
+	const [HttpError, setHttpError] = useState("");
+	useEffect(() => {
+		const fetchData = async () => {
+			const response = await fetch("https://rickandmortyapi.com/api/character");
+			// setHttpError(null);
+			if (!response.ok) {
+				throw new Error("something went wrong");
+			}
+
+			const data = await response.json();
+			setLoadedData(data);
+			console.log(data);
+		};
+		fetchData().catch((error) => {
+			setHttpError(error.message);
+		});
+	}, []);
+	console.log(loadedData);
+
+	const caractersData = loadedData?.results;
+
+	const caracterList = caractersData?.map((caracter, i) => {
+		return (
+			<CharacterCard
+				key={i}
+				name={caracter.name}
+				gender={caracter.gender}
+				image={caracter.image}
+			/>
+		);
+	});
+
 	return (
 		<>
 			<Header />
-			<main className="flex flex-col  justify-start bg-slate-800 text-white p-2 lg:px-6 mt-16  h-[calc(100vh-4rem)]  ">
+			<main className="flex flex-col  justify-start bg-slate-800 text-white p-2 lg:px-6 mt-16  h-  ">
+				{/* [calc(100vh-4rem)] */}
 				<div className="my-4 bg-red-500 h-16  flex  items-center">
 					<SearchBar />
 				</div>
 				<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  justify-items-center mx-auto max-w-7xl gap-8  ">
-					<CharacterCard />
+					{HttpError && <p className="col-start-2">{HttpError}</p>}
+
+					{caracterList}
 				</div>
 			</main>
 		</>
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -3,20 +3,21 @@ import { FaMale } from "react-icons/fa";
 import { FaFemale } from "react-icons/fa";
 import { BsInfoLg } from "react-icons/bs";
 
-const CharacterCard = () => {
-	const imageUrl = "https://rickandmortyapi.com/api/character/avatar/1.jpeg";
+const CharacterCard = ({ name, gender, image }) => {
+	const imageUrl = image;
 	const isMale = "male";
+	console.log(name, gender);
 	return (
 		<div className="relative h-64 w-64 mb-8">
 			<img src={`${imageUrl}`} alt="character" className="rounded-xl" />
 			<div className=" absolute  -bottom-10 left-1/2 rounded-lg -translate-x-1/2 w-10/12 overflow-hidden  ">
 				<div className="bg-slate-700 px-2">
-					<h3 className="  text-2xl text-center  ">name</h3>
+					<h3 className="  text-2xl text-center  ">{name}</h3>
 				</div>
 				<div className="bg-slate-600 flex p-2 justify-between ">
 					<div className="flex items-center">
 						{isMale == "male" ? <FaMale /> : <FaFemale />}
-						<p>gender</p>
+						<p>{gender}</p>
 					</div>
 					<div className="flex bg-slate-700 rounded-md px-1 items-center">
 						<BsInfoLg />
