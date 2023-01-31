41df31b2ca50e8d04927484d3ddeba36b21e1ef8
OSAMA SHAIKH <shaikhosama478@gmail.com>
2023-01-31 13:41:35 +0530

    ✨ feat: header added

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<header className="fixed top-0 h-16 z-50 bg-slate-700 w-full text-white flex justify-start items-center p-2 lg:px-6">
+			<h1 className="capitalize text-4xl font-bold">hatch</h1>
+		</header>
+	);
+};
+
+export default Header;
