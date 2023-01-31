2d7e68f0e30e18b8e96e46af7c3094a91f74bf83
OSAMA SHAIKH <shaikhosama478@gmail.com>
2023-01-31 13:42:54 +0530

    ✨ feat: main section added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import { SiTailwindcss } from "react-icons/si";
+import CharacterCard from "./components/CharacterCard";
+import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
 
 const App = () => {
 	return (
-		<main className="flex items-center justify-center h-screen">
-			<h1 className="text-4xl">rick and morty all character.</h1>
-		</main>
+		<>
+			<Header />
+			<main className="flex flex-col  justify-start bg-slate-800 text-white p-2 lg:px-6 mt-16  h-[calc(100vh-4rem)]  ">
+				<div className="my-4 bg-red-500 h-16  flex  items-center">
+					<SearchBar />
+				</div>
+				<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  justify-items-center mx-auto max-w-7xl gap-8  ">
+					<CharacterCard />
+				</div>
+			</main>
+		</>
 	);
 };
 
