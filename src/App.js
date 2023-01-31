083b4d84df14451e2fcade86a3738386629d0175
OSAMA SHAIKH <shaikhosama478@gmail.com>
2023-01-31 10:58:40 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { SiTailwindcss } from "react-icons/si";
+
+const App = () => {
+	return (
+		<main className="flex items-center justify-center h-screen">
+			<h1 className="text-4xl">rick and morty all character.</h1>
+		</main>
+	);
+};
+
+export default App;
