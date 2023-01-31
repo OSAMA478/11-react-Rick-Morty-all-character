138f30ece44956da6b2c69140a44cfa07502a467
OSAMA SHAIKH <shaikhosama478@gmail.com>
2023-01-31 18:00:58 +0530

    ✅ prev next button added

--- src/App.js
+++ src/App.js
@@ -8,9 +8,15 @@ const App = () => {
 	const [loadedData, setLoadedData] = useState({});
 	// const [isLoading, setIsLoading] = useState(false);
 	const [HttpError, setHttpError] = useState("");
+	const [fetchUrl, setFetchUrl] = useState(
+		"https://rickandmortyapi.com/api/character"
+	);
+
+	const nextUrl = loadedData?.info?.next;
+	const prevUrl = loadedData?.info?.prev;
 	useEffect(() => {
 		const fetchData = async () => {
-			const response = await fetch("https://rickandmortyapi.com/api/character");
+			const response = await fetch(fetchUrl);
 			// setHttpError(null);
 			if (!response.ok) {
 				throw new Error("something went wrong");
@@ -23,7 +29,7 @@ const App = () => {
 		fetchData().catch((error) => {
 			setHttpError(error.message);
 		});
-	}, []);
+	}, [fetchUrl]);
 	console.log(loadedData);
 
 	const caractersData = loadedData?.results;
@@ -47,10 +53,28 @@ const App = () => {
 				<div className="my-4 bg-red-500 h-16  flex  items-center">
 					<SearchBar />
 				</div>
-				<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  justify-items-center mx-auto max-w-7xl gap-8  ">
+				<div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  justify-items-center mx-auto max-w-7xl gap-8  ">
 					{HttpError && <p className="col-start-2">{HttpError}</p>}
 
 					{caracterList}
+				</div>
+				<div className="mx-auto flex gap-6 pt-10 pb-4">
+					<button
+						onClick={() => {
+							setFetchUrl(prevUrl);
+						}}
+						className="bg-slate-700 px-8 py-2 rounded-md text-lg"
+					>
+						Prev
+					</button>
+					<button
+						onClick={() => {
+							setFetchUrl(nextUrl);
+						}}
+						className="bg-slate-700 px-8 py-2 rounded-md text-lg"
+					>
+						Next
+					</button>
 				</div>
 			</main>
 		</>
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -8,11 +8,15 @@ const CharacterCard = ({ name, gender, image }) => {
 	const isMale = "male";
 	console.log(name, gender);
 	return (
-		<div className="relative h-64 w-64 mb-8">
-			<img src={`${imageUrl}`} alt="character" className="rounded-xl" />
+		<div className=" relative h-64 w-64 mb-8 hover:scale-105 transition duration-200">
+			<img
+				src={`${imageUrl}`}
+				alt="character"
+				className="rounded-xl hover:shadow-lg  hover:shadow-white transition duration-200"
+			/>
 			<div className=" absolute  -bottom-10 left-1/2 rounded-lg -translate-x-1/2 w-10/12 overflow-hidden  ">
 				<div className="bg-slate-700 px-2">
-					<h3 className="  text-2xl text-center  ">{name}</h3>
+					<h3 className="  text-xl text-center  ">{name}</h3>
 				</div>
 				<div className="bg-slate-600 flex p-2 justify-between ">
 					<div className="flex items-center">
